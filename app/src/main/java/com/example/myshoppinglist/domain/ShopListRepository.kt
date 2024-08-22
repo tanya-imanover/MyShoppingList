da8b3cb412f7125084a47bf1bf14e6e0414f7c90
TI <tanya.imanover@gmail.com>
2024-08-22 16:06:31 +0700

    Fix edit shopItem problem, add MainViewModel

--- app/src/main/java/com/example/myshoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/myshoppinglist/domain/ShopListRepository.kt
@@ -1,5 +1,7 @@
 package com.example.myshoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
 
     fun addShopItem(item: ShopItem)
@@ -8,7 +10,7 @@ interface ShopListRepository {
 
     fun getShopItem(id: Int) : ShopItem
 
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 
     fun removeShopItem(shopItem: ShopItem)
 }
